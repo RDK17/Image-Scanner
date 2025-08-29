b2c24447623f0119a3f8ad298b6bd40f381d1de4
Roger Kozlyuk <rkozlyuk8@gmail.com>
2025-08-29 15:42:31 -0700

    Update tools.py
    - using end_d instead of set_d
    
    - networkx to partition the set of contours
    
    - too slow to add to pyscan(would decrease fps to < 1)

--- tools.py
+++ tools.py
@@ -1,16 +1,18 @@
 import numpy as np
 import cv2 as cv
+import networkx as nx 
 
-def set_d(A,B):
+def end_d(A,B, dst_only = True):
     d = lambda x,y: np.linalg.norm(x-y)
-    dst = d(A[0],B[0])
-    pts = (0,0)
-    for a in A:
-        for b in B:
-            curr_dst = d(a,b)
-            if curr_dst < dst:
-                dst = curr_dst
-                pts = (a, b)
+    ends = [[A[-1,0],B[-1,0]],[A[-1,0],B[0,0]],[A[0,0],B[-1,0]],[A[0,0],B[0,0]]]
+    get_d = lambda p: d(p[0],p[1])
+    if dst_only:
+        ds = [get_d(p) for p in ends]
+        dst = min(ds)
+        return dst
+    ends = sorted(ends, key = get_d)
+    pts = ends[0]
+    dst = d(pts[0],pts[1])
     return dst, pts
 
 def get_contours(img):
@@ -22,20 +24,46 @@ def get_contours(img):
     contours, _ = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     return contours
 
+def get_density_graph(contours, d, epsilon):
+    #brute force find edges in contours where an edge between x and y must have d(x,y) < epsilon
+    #return visited cnts(stored as a set) as an array
+    G = nx.Graph()
+    n = len(contours)
+    G.add_nodes_from(range(n)) 
+    for i in range(n):
+        for j in range(n):
+            #print(i,j)
+            if i!=j and d(contours[i], contours[j]) < epsilon:
+                G.add_edge(i,j)
+    return G
+
+
+def partition(img, d, epsilon):
+    contours = get_contours(img)
+    G = get_density_graph(contours,d,epsilon)
+    prtn = list(nx.connected_components(G))
+    return prtn
+
+
+
 img = cv.imread("GOBRUINSW.jpeg")
-contours = get_contours(img)
 
-n1, n2 = np.random.randint(0,len(contours),size = 2)
-cnt1 = contours[n1]
-cnt2 = contours[n2]
+##contours = get_contours(img)
+##
+##n1, n2 = np.random.randint(0,len(contours),size = 2)
+##cnt1 = contours[n1]
+##cnt2 = contours[n2]
+##
+##min_pts = set_d(cnt1,cnt2)[1]
+##min_dst = set_d(cnt1, cnt2)[0]
+##print(f"distance between contours: {min_dst}")
+##
+##black_img = np.zeros(img.shape)
+##cv.drawContours(black_img, [cnt1,cnt2] , -1, (0,255,0), 4)
+##cv.line(black_img, min_pts[0][0], min_pts[1][0], (0,0,255), 2)
+##cv.imshow("contours", black_img)
+##cv.waitKey(5000)
+##cv.destroyAllWindows()
 
-min_pts = set_d(cnt1,cnt2)[1]
-min_dst = set_d(cnt1, cnt2)[0]
-print(f"distance between contours: {min_dst}")
-
-black_img = np.zeros(img.shape)
-cv.drawContours(black_img, [cnt1,cnt2] , -1, (0,255,0), 4)
-cv.line(black_img, min_pts[0][0], min_pts[1][0], (0,0,255), 2)
-cv.imshow("contours", black_img)
-cv.waitKey(5000)
-cv.destroyAllWindows()
+#print(len(get_contours(img)))
+partition(img, end_d, 8.0)
