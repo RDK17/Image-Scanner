b69d6bf9ee108b5012f19cf75110b2603f1a08d8
Roger Kozlyuk <rkozlyuk8@gmail.com>
2025-09-08 21:37:08 -0700

    Update pyscan.py
    ADDED SMOOTH SCANNING FEATURE
    - almost no misclassification when image in frame(not tested)
    - HUGE improvement from previous naive classification
    - using get_box rn but will switch to get_quad after I fix it

--- pyscan.py
+++ pyscan.py
@@ -1,6 +1,9 @@
 import cv2 as cv
 import numpy as np
-from tools import get_fit_isometry
+import networkx as nx
+import tools as t
+from collections import deque
+import graphs
 
 def take_picture(outline = False, avg_contours = False): #take a picture
     cap = cv.VideoCapture(0)
@@ -8,24 +11,38 @@ def take_picture(outline = False, avg_contours = False): #take a picture
     img = np.zeros(img_shape)
     tot_contours = 0
     n = 0
+    frame_num = 0
+    quads = deque()
+    hd = cv.createHausdorffDistanceExtractor()
+    d_H = lambda x, y: t.safe_hd(hd,x,y)
+
     while True:
 
         ret, frame = cap.read()
-
+        quad = get_box(frame, ret_type = "points").reshape(-1,1,2)
+        quads.append(quad)
+        if len(quads) > 5:#add memory param w default 5?
+            quads.popleft()
+       
         if outline:
             picture = img_of_contours(frame)
             cv.imshow("contours", picture)
         else:
-            picture = frame
-            picture_box = get_box(picture)
-            if get_quad(picture) is not None:
-                if len(get_quad(picture)) == 4:
-                    scan(picture, t=1)
-
-            cv.imshow("scanning...", picture_box)
+            G = t.cnts2ENG(quads, 10, t.my_hd)
+            box = t.get_voted_cnt(G)
+            box = t.tup2cnt(box)
+            frame_c = frame.copy()
+            cv.drawContours(frame_c, [box], 0 ,(255,0,0), 2)
+            frame_num +=1
+            frame_box = frame_c
+            if frame_num%15 == 0:
+                None
+                #print(f"quad info: quad = {quad}, size = {quad.size}, shape = {quad.shape}")
+            cv.imshow("scanning...", frame_box)
+            
 
         if avg_contours:
-            tot_contours += len(get_contours(picture))
+            tot_contours += len(get_contours(frame))
             n+=1
         
         key = cv.waitKey(1) & 0xFF
@@ -33,7 +50,7 @@ def take_picture(outline = False, avg_contours = False): #take a picture
             break
 
         if key == ord('c'):
-            img = picture
+            img = frame
             cv.imshow("img", img)
             cv.waitKey(2000)
             cv.destroyWindow("img")
@@ -45,11 +62,11 @@ def take_picture(outline = False, avg_contours = False): #take a picture
             _, current_frame = cap.read()
             scan(current_frame)
 
-
     if avg_contours:
         print(f"average number of contours detected per frame: {int(tot_contours/n)}")
     cap.release()
     cv.destroyAllWindows()
+
 
 def get_contours(img):
     img_c = img.copy()
@@ -62,6 +79,7 @@ def get_contours(img):
     edged = cv.Canny(gray, .66*mid, 1.33*mid)
     contours, _ = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     return contours
+
 
 def img_of_contours(img, f = get_contours, all_cnt = True, show_min_rect = False, show_poly = False, view = 0):
     if all_cnt:    
@@ -86,12 +104,14 @@ def img_of_contours(img, f = get_contours, all_cnt = True, show_min_rect = False
         cv.drawContours(black_img, [poly], -1, (0,255,0), 3)
     return black_img
 
+
 def view_contours(img, n, show_min_rect = False, show_poly = False):
         imgs = [img_of_contours(img, all_cnt = False, show_min_rect = show_min_rect, show_poly = show_poly, view = i) for i in range(n)]
         for i in range(n):
             cv.imshow("cnt", imgs[i])
             cv.waitKey(1000)
             cv.destroyWindow("cnt")
+
 
 def get_box(img, ret_type = "image"):
     contours = get_contours(img)
@@ -108,7 +128,8 @@ def get_box(img, ret_type = "image"):
     elif ret_type == "points":
         return box
 
-def get_quad(img, ret_type ="image"):
+
+def get_quad(img, ret_type ="image"):#BROKEN
     box = get_box(img, "points")
 
     if box is None or len(box)< 4:
@@ -123,7 +144,7 @@ def get_quad(img, ret_type ="image"):
         tr = box[np.argmin(diff)]
         bl = box[np.argmax(diff)]
         pts1 = np.array([tl, tr, br, bl], dtype = np.float32)
-        M, width, height = get_fit_isometry(pts1, scale = 15, get_dims = True)
+        M, width, height = t.get_fit_isometry(pts1, scale = 15, get_dims = True)
         new_img = cv.warpPerspective(img,M,(int(width),int(height)))
         dst = cv.cvtColor(new_img,cv.COLOR_BGR2GRAY)
         dst = cv.GaussianBlur(dst,(5,5),0)
@@ -146,18 +167,25 @@ def get_quad(img, ret_type ="image"):
         print("[ERROR] in get_quad:", e)
         return None
 
-def scan(img, t=2):
-    pts1 = get_quad(img, ret_type = "points")
-    if get_fit_isometry(pts1) is not None:
-        M, width, height = get_fit_isometry(pts1, scale = -10, get_dims = True)
+
+def scan(img, wait=2, save = False):
+    pts1 = get_box(img, ret_type = "points")
+    if t.get_fit_isometry(pts1) is not None:
+        M, width, height = t.get_fit_isometry(pts1, scale = -10, get_dims = True)
         print(M, width, height)
         new_img = cv.warpPerspective(img,M,(int(width),int(height)))
     else:
         print("couldn't fit isometry")
         new_img = img
-    cv.imshow("scanned", new_img)
-    cv.waitKey(t*1000)
+    gray_img = cv.cvtColor(new_img, cv.COLOR_BGR2GRAY)
+    scanned_img = cv.adaptiveThreshold(gray_img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
+    cv.imshow("scanned", scanned_img)
+    cv.waitKey(wait*1000)
     cv.destroyWindow("scanned")
+    if save:
+        cv.imwrite("test_unscanned.jpg",scanned_img)
 
 
-take_picture(False, False)
+if __name__ == "__main__":
+    take_picture(False, False)
+
